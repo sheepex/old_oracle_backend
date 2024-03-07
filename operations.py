80f742f8152a155b746d707763ba2ca387ad13e8
Elói da Silva <eloi@eloicarneiro.com>
2024-03-07 15:50:27 -0300

    bugfix na função _get_sequence_name

--- operations.py
+++ operations.py
@@ -571,14 +571,18 @@ WHEN (new.%(col_name)s IS NULL)
         name_length = self.max_name_length() - 3
         return '%s_SQ' % truncate_name(strip_quotes(table), name_length).upper()
 
-    def _get_sequence_name(self, cursor, table, pk_name):
-        cursor.execute("""
-            SELECT sequence_name
-            FROM user_tab_identity_cols
-            WHERE table_name = UPPER(%s)
-            AND column_name = UPPER(%s)""", [table, pk_name])
-        row = cursor.fetchone()
-        return self._get_no_autofield_sequence_name(table) if row is None else row[0]
+    #def _get_sequence_name(self, cursor, table, pk_name):
+        #cursor.execute("""
+            #SELECT sequence_name
+            #FROM user_tab_identity_cols
+            #WHERE table_name = UPPER(%s)
+            #AND column_name = UPPER(%s)""", [table, pk_name])
+        #row = cursor.fetchone()
+        #return self._get_no_autofield_sequence_name(table) if row is None else row[0]
+
+    def _get_sequence_name(self, table):
+        name_length = self.max_name_length() - 3
+        return '%s_SQ' % truncate_name(strip_quotes(table), name_length).upper()
 
     def _get_trigger_name(self, table):
         name_length = self.max_name_length() - 3
